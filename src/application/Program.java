01613437fc66e92451d8540116754808a7797b28
Vinicius Florentino <vinicesar2912@gmail.com>
2023-08-07 00:39:05 -0300

    Update and delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,7 +12,7 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
@@ -31,6 +32,20 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com",new Date(),4000.0,department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted new id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update!");
+		
+		System.out.println("\n=== TEST 6: seller delete ===");
+		System.out.print("Enter id for delete test:");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 	}
 
 }
